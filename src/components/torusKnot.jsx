09b1aed0dfd1b0fc20b1c58b1919d9c57a090d39
vidit <vidit.gautam.vsv@gmail.com>
2024-10-15 00:09:01 +0530

    ObitControl added

--- src/components/torusKnot.jsx
+++ src/components/torusKnot.jsx
@@ -1,5 +1,9 @@
-import {useFrame} from "@react-three/fiber";
+import {useFrame,useThree, extend} from "@react-three/fiber";
 import {useRef} from "react";
+import * as THREE from "three";
+import {OrbitControls} from "three/addons/controls/OrbitControls.js";
+
+extend({OrbitControls:OrbitControls})
 
 const TorusKnot=()=> {
     const cubRef=useRef()
@@ -9,11 +13,15 @@ const TorusKnot=()=> {
         cub2.current.rotation.y+=delta
 
     })
+
+    const three=useThree()
+    console.log(three)
     return (
         <>
-            <mesh ref={cubRef} position-x={[-3]} rotation-y={Math.PI* 0.25}>
-                <boxGeometry args={[1, 1, 1]}/>
-                <meshBasicMaterial color='blue'/>
+            <orbitControls args={[three.camera,three.gl.domElement]} />
+            <mesh ref={cubRef} position-x={[-3]} rotation-y={Math.PI* 1}>
+                <planeGeometry args={[1, 1, 1]}/>
+                <meshBasicMaterial color='blue' side={THREE.DoubleSide}/>
             </mesh>
             <mesh ref={cub2} position-x={[3]} rotation-y={Math.PI* 0.25}>
                 <boxGeometry args={[3, 3, 3]}/>
